b192cc89b9ce3467795e8f60f5c237e1979cb349
KomaWasTaken <valentin.gaillard-21@outlook.fr>
2018-07-18 09:27:27 +0200

    Oops, petit problème de routes

--- app/kernel/Router.php
+++ app/kernel/Router.php
@@ -1,25 +1,27 @@
 <?php
-
 class Router {
-
     public static function analyse($request){
         $result = array(
             'controller'    => 'Error',
             'action'        => 'error404',
             'params'        => array()
         );
-
         if($request === '' || $request === '/'){ // Route vers la page d'accueil
             $result['controller']   = 'Page';
             $result['action']       = 'display';
         } else {
         $parts = explode("/", $request);
-        if($parts[0] == "produit" && count($parts) == 2) {//Route vers la page produit
+        if($parts[0] == "produit" && count($parts) == 2) {//route vers fiche film
            $result["controller"] = "Page";
            $result["action"] = "produit";
            $result["params"]["id"] = $parts[1];
                      
         }
+        if($parts[0] == "tricafe") {//route vers fiche film
+            $result["controller"] = "Page";
+            $result["action"] = "tricafe";
+        }
+    }
      return $result;
     }  
 }
